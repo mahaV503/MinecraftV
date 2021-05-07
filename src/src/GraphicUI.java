ee2d6ced0ddea824db2ca3b3d2fcbe0349c45cc6
Mahavishnu <mahavishnureddya@gmail.com>
2021-05-07 18:59:57 -0400

    Logic for Tiles

--- src/src/GraphicUI.java
+++ src/src/GraphicUI.java
@@ -61,4 +61,46 @@ class Click implements MouseListener {
     public void mouseExited(MouseEvent e) {
 
     }
+
+    int vicX = -250;
+    int vicY = 25;
+    int vicX2 = -250;
+
+    int[][] table = new int[16][12];
+    int[][] nearby = new int[16][12];
+    boolean[][] revealed = new boolean[16][12];
+    boolean[][] flagged = new boolean[16][12];
+
+    Random rand = new Random();
+
+    boolean flagger = false;
+
+    boolean happy = true;
+
+    public void GameClass() {
+
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 12; j++) {
+                table[i][j] = 0;
+                nearby[i][j] = 0;
+                revealed[i][j] = false;
+                flagged[i][j] = false;
+                if (rand.nextInt(100) < 20 && j > 1) {
+                    table[i][j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 12; j++) {
+                for (int m = 0; m < 16; m++) {
+                    for (int n = 0; n < 12; n++) {
+                        if (Math.abs(i-m) <= 1 && Math.abs(j-n) <= 1 && (i != m || j != n) && table[m][n] == 1) {
+                            nearby[i][j]++;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
